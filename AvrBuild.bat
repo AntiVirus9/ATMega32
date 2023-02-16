6ea2bf6afb0db44995eb6056e9a10234cc20cd25
Pavel Chludil <74764576+AntiVirus9@users.noreply.github.com>
2023-02-16 10:09:40 +0100

    Delete AvrBuild.bat

--- AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "R:\0Vyukove_slozky\IT_04_MIT\LCD01\labels.tmp" -fI -W+ie -C V2E -o "R:\0Vyukove_slozky\IT_04_MIT\LCD01\LCD01.hex" -d "R:\0Vyukove_slozky\IT_04_MIT\LCD01\LCD01.obj" -e "R:\0Vyukove_slozky\IT_04_MIT\LCD01\LCD01.eep" -m "R:\0Vyukove_slozky\IT_04_MIT\LCD01\LCD01.map" "R:\0Vyukove_slozky\IT_04_MIT\LCD01\LCD01.asm"
